0189b4fee95b430728680ab1c03a994111c7c39a
Adams Mohammed <moadams847@gmail.com>
2022-01-05 19:18:54 +0000

    update views

--- pic_drive/pics/models.py
+++ pic_drive/pics/models.py
@@ -9,18 +9,19 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 class Picture(models.Model):
-    image = models.ImageField(default='default.jpg', upload_to='pictures')
+    image = models.ImageField(upload_to='pictures')
     date_posted = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, default='General')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default='General', blank=True)
     
     def __str__(self):
         return self.image.name
     
 class DeletePicture(models.Model):
-    image = models.ImageField(default='default.jpg', upload_to='pictures')
+    image = models.ImageField(upload_to='pictures')
     date_deleted = models.DateTimeField(auto_now_add=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, default='General')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default='General', blank=True)
     
     def __str__(self):
         return self.image.name
