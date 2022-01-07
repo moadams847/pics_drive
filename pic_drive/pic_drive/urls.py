3f640ad1239343cd21281a16a36caec69a3cf35f
Adams Mohammed <moadams847@gmail.com>
2022-01-07 08:42:08 +0000

    update user/views.py

--- pic_drive/pic_drive/urls.py
+++ pic_drive/pic_drive/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pics.urls')),
     
-     path('register/', user_views.register, name='register'),
+     path('register/', user_views.RegisterPage.as_view(), name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html', next_page = 'login'), name='logout'),
     
