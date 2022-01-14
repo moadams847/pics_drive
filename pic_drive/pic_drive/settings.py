39cd73c5288800ed81f86ebafa0f59347d42218e
Adams Mohammed <moadams847@gmail.com>
2022-01-14 10:36:08 +0000

    set debug value

--- pic_drive/pic_drive/settings.py
+++ pic_drive/pic_drive/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = (config('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['picsdrive.herokuapp.com']
 
